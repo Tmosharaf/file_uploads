15c01e1174268eca0be82705980490e51841f185
Mohammad Mosharaf Tanvir <6365213+Tmosharaf@users.noreply.github.com>
2020-10-26 20:18:46 +0600

    Added Bootstrap Css

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php  ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>File Upload</title>
+  </head>
+  <body>
+    <form class="upload_form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <input type="file" name="file" >
+      <input type="submit" name="submit" value="submit">
+    </form>
+  </body>
+</html>
